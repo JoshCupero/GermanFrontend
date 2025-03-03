284b8ae64f700db4c0f2f592b6acf88d6ffc95d0
Josh Cupero <JoshuaCupero@gmail.com>
2025-03-03 02:46:57 -0500

    Improved design and added new features

--- script.js
+++ script.js
@@ -1,13 +1,36 @@
 const likeCountElement = document.getElementById("likeCount");
 const likeButton = document.getElementById("likeButton");
 
-// Get likes from localStorage or set to 0
+// Load likes from localStorage or default to 0
 let likes = localStorage.getItem("likes") ? parseInt(localStorage.getItem("likes")) : 0;
 likeCountElement.innerText = likes;
 
-// Increment likes on button click
+// Function to update likes
 likeButton.addEventListener("click", () => {
     likes++;
     localStorage.setItem("likes", likes);
     likeCountElement.innerText = likes;
 });
+
+document.querySelectorAll('nav a').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        const targetId = this.getAttribute('href').substring(1);
+        document.getElementById(targetId).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+const nameText = "Josh Cupero";
+let index = 0;
+function typeEffect() {
+    if (index < nameText.length) {
+        document.getElementById("typingEffect").innerText += nameText[index];
+        index++;
+        setTimeout(typeEffect, 100);
+    }
+}
+typeEffect();
+
+
